a3774f513d1910a4bf7a974c56a9285ab3c7e093
Dyeseniact <camposdyes@gmail.com>
2023-10-16 20:04:39 -0600

    Sesion 04 - Reto 01: Cálculo de las ganancias diarias para un conjunto de franquicias

--- s4/src/main/java/org/example/CadenaRestaurantes.java
+++ s4/src/main/java/org/example/CadenaRestaurantes.java
@@ -1,2 +1,17 @@
-package org.example;public class CadenaRestaurantes {
+package org.example;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class CadenaRestaurantes {
+    private Random random = new Random();
+
+    public double obtenerGananciasFranquicia(int id) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return random.nextInt(40000) + 10000;
+    }
 }
--- s4/src/main/java/org/example/R1.java
+++ s4/src/main/java/org/example/R1.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class R1 {
+    public static void main(String[] args) {
+        List<Integer> ids = IntStream.range(0, 1000)
+                .boxed()
+                .collect(Collectors.toList());
+        obtenerSuma(ids.parallelStream());
+    }
+
+    private static void obtenerSuma(Stream<Integer> ids) {
+        LocalTime inicio = LocalTime.now();
+        double suma = ids.mapToDouble(id -> new CadenaRestaurantes()
+                        .obtenerGananciasFranquicia(id))
+                .sum();
+        System.out.println("Ganancias totales: " + String.format("$%.2f", suma));
+        Duration tiempo = Duration.between(inicio, LocalTime.now());
+        System.out.println((Math.round(suma * 100.) / 100.) + " en " + tiempo.toMillis() + " ms");
+    }
 }
