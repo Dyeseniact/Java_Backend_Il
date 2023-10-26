cadfa140e2f6e7557506e0afd55487cccfcb26b6
Dyeseniact <camposdyes@gmail.com>
2023-10-25 20:04:30 -0600

    Reto 01

--- s7.2/src/main/java/com/example/s72/controller/DemoController.java
+++ s7.2/src/main/java/com/example/s72/controller/DemoController.java
@@ -1,6 +1,8 @@
 package com.example.s72.controller;
 
 import com.example.s72.entity.Auto;
+import com.example.s72.entity.Casa;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,5 +14,11 @@ public class DemoController {
     @PostMapping("/auto")
     public void recibe(@RequestBody Auto auto){
         System.out.println(auto.toString());
+    }
+
+    @PostMapping("/casa")
+    public ResponseEntity<Casa> recibeCasa(@RequestBody Casa casa){
+        System.out.println(casa.toString());
+        return ResponseEntity.status(200).body(casa);
     }
 }
